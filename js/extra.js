aecd930e0ec42790cfb86010d2b17167b02c423e
emonahamed <emonahamed711@gmail.com>
2022-09-03 08:47:05 +0600

    fourth commit

--- js/extra.js
+++ js/extra.js
@@ -9,12 +9,37 @@ const myFunction = async () => {
 
 const displayCatagories = (cards) => {
     // console.log(cards);
-    // console.log('p');
 
     const cardContainer = document.getElementById('card-container')
 
     cards.forEach(card => {
-        console.log(card);
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col-12');
+        cardDiv.innerHTML = `
+        <div class="card mb-3" style="max-width: 740px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="..." class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural
+                        lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between">
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small>
+                        </p>
+                        <p class="card-text"><small class="text-muted">done</small>
+                        </p>
+                        <p class="btn btn-light">Open Details</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        `;
+
+        cardContainer.appendChild(cardDiv);
 
 
 
